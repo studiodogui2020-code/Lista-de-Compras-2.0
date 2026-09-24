42a5fe139d5231d6cb17c60a3550ed8a92d6ccf7
studiodogui2020-code <studiodogui2020@gmail.com>
2026-09-24 15:27:25 +0000

    em  andamento

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 import { StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react"
 import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
 import ListaItens from "./components/ListaItens/ListaItens";
 import { colors } from "./components/colors";
+import { ProdutoItem } from "./interfaces/ProdutoItem";
 
 export default function App() {
+  const [produtos, setProdutos] = useState<ProdutoItem[]>([]);
+
+  function adicionarProduto(nome: string) {
+    const novoProduto: ProdutoItem = {
+      id: Date.now().toString(),
+      nome,
+      comprado: false,
+    };
+    setProdutos((listaAtual) => [...listaAtual, novoProduto]);
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <StatusBar style="auto" />
         <Header />
-        <Form />
+        <Form onAdicionar={adicionarProduto} />
         <ListaItens />
       </SafeAreaView>
     </SafeAreaProvider>
